fa065013b67551008336fdde9bfbd4323fda2f0d
oatThanut <thanut.sjknk@gmail.com>
2018-01-18 16:44:42 +0700

    Initial Commit

---
+++ BMICalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by oatThanut on 18/1/2561 BE.
+//  Copyright © 2561 oatThanut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
