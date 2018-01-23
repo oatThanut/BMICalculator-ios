a868865ddf695b6b1879d43503f3ca68c8287aee
oatThanut <thanut.sjknk@gmail.com>
2018-01-24 00:17:24 +0700

    Upgrade weight and height value from label to textField and refactor code

--- BMICalculator/ViewController.swift
+++ BMICalculator/ViewController.swift
@@ -13,75 +13,119 @@ class ViewController: UIViewController {
     @IBOutlet weak var ImageView: UIImageView!
     @IBOutlet weak var stageLabel: UILabel!
     @IBOutlet weak var BMIValue: UILabel!
-    @IBOutlet weak var WeightLabel: UILabel!
-    @IBOutlet weak var HeightLabel: UILabel!
+    @IBOutlet weak var WeightTextFiled: UITextField!
+    @IBOutlet weak var HeightTextField: UITextField!
+    @IBOutlet weak var WeightSlider: UISlider!
+    @IBOutlet weak var HeightSlider: UISlider!
     @IBOutlet weak var Background: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        WeightTextFiled.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        HeightTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
     }
 
     @IBAction func WeightSlider(_ sender: UISlider) {
         let currentWeight = Double(sender.value)
-        WeightLabel.text = String(format: "%.1f", currentWeight)
+        WeightTextFiled.text = String(format: "%.1f", currentWeight)
         calculate()
     }
     
     @IBAction func HeightSlider(_ sender: UISlider) {
         let currentHeight = Double(sender.value)
-        HeightLabel.text = String(format: "%.1f", currentHeight)
+        HeightTextField.text = String(format: "%.1f", currentHeight)
+        calculate()
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        if textField.tag == 1 {
+            if let value = Float(WeightTextFiled.text!){
+                WeightSlider.setValue(value, animated: true)
+            }
+        } else {
+            if let value = Float(HeightTextField.text!){
+                HeightSlider.setValue(value, animated: true)
+            }
+        }
         calculate()
     }
     
     func calculate() {
-        if let weight = Double(WeightLabel.text!), let height = Double(HeightLabel.text!) {
+        if let weight = Double(WeightTextFiled.text!), let height = Double(HeightTextField.text!) {
             if weight != 0, height != 0 {
                 let result = String(format: "%.1f", weight/pow((height/100), 2))
                 BMIValue.text = result
                 changeStage(BMI: result)
             } else {
-                stageLabel.text = "BMI Calculator"
-                BMIValue.text = "0.0"
-                ImageView.image = UIImage(named: "Logo")
-                Background.backgroundColor =  UIColor(red: 0.102,
-                                                      green: 0.2902,
-                                                      blue: 0.4392,
-                                                      alpha: 1)
+                updateStage(stage: "default")
             }
+        } else if WeightTextFiled.text == "" || HeightTextField.text == "" {
+            updateStage(stage: "default")
+        } else {
+            updateStage(stage: "Error")
         }
     }
     
     func changeStage(BMI:String) {
         if let bmi:Double = Double(BMI) {
             if bmi <= 18.5 {
-                stageLabel.text = "Underweight"
-                ImageView.image = UIImage(named: "Underweight")
-                Background.backgroundColor =  UIColor(red: 0.1529,
-                                                      green: 0.9686,
-                                                      blue: 0.7765,
-                                                      alpha: 1)
+                updateStage(stage: "Underweight")
             } else if bmi <= 24.29 {
-                stageLabel.text = "Healthy"
-                ImageView.image = UIImage(named: "Healthy")
-                Background.backgroundColor =  UIColor(red: 0.1373,
-                                                      green: 0.8667,
-                                                      blue: 0.3216,
-                                                      alpha: 1)
+                updateStage(stage: "Healthy")
             } else if bmi <= 29.9 {
-                stageLabel.text = "Overweight"
-                ImageView.image = UIImage(named: "Overweight")
-                Background.backgroundColor =  UIColor(red: 0.9098,
-                                                      green: 0.7686,
-                                                      blue: 0.1451,
-                                                      alpha: 1)
+                updateStage(stage: "Overweight")
             } else {
-                stageLabel.text = "Obese"
-                ImageView.image = UIImage(named: "Obese")
-                Background.backgroundColor =  UIColor(red: 0.8392,
-                                                      green: 0.1333,
-                                                      blue: 0.1333,
-                                                      alpha: 1)
+                updateStage(stage: "Obese")
             }
+        }
+    }
+    
+    func updateStage(stage:String) {
+        if stage == "Error" {
+            stageLabel.text = "BMI Calculator"
+            BMIValue.text = "Error"
+            ImageView.image = UIImage(named: "Logo")
+            Background.backgroundColor =  UIColor(red: 0.102,
+                                                  green: 0.2902,
+                                                  blue: 0.4392,
+                                                  alpha: 1)
+        }else if stage == "Underweight" {
+            stageLabel.text = "Underweight"
+            ImageView.image = UIImage(named: "Underweight")
+            Background.backgroundColor =  UIColor(red: 0.1529,
+                                                  green: 0.9686,
+                                                  blue: 0.7765,
+                                                  alpha: 1)
+        }else if stage == "Healthy" {
+            stageLabel.text = "Healthy"
+            ImageView.image = UIImage(named: "Healthy")
+            Background.backgroundColor =  UIColor(red: 0.1373,
+                                                  green: 0.8667,
+                                                  blue: 0.3216,
+                                                  alpha: 1)
+        }else if stage == "Overweight" {
+            stageLabel.text = "Overweight"
+            ImageView.image = UIImage(named: "Overweight")
+            Background.backgroundColor =  UIColor(red: 0.9098,
+                                                  green: 0.7686,
+                                                  blue: 0.1451,
+                                                  alpha: 1)
+        }else if stage == "Obese" {
+            stageLabel.text = "Obese"
+            ImageView.image = UIImage(named: "Obese")
+            Background.backgroundColor =  UIColor(red: 0.8392,
+                                                  green: 0.1333,
+                                                  blue: 0.1333,
+                                                  alpha: 1)
+        } else {
+            stageLabel.text = "BMI Calculator"
+            BMIValue.text = "0.0"
+            ImageView.image = UIImage(named: "Logo")
+            Background.backgroundColor =  UIColor(red: 0.102,
+                                                  green: 0.2902,
+                                                  blue: 0.4392,
+                                                  alpha: 1)
         }
     }
 }
